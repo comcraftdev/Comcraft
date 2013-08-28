febd1ddd3f0aa981714c1938310c76e35d41c641
simon816 <simonbarnes1@googlemail.com>
2013-08-28 22:16:39 +0100

    Organizes the mod loading

--- src/net/comcraft/src/Mod.java
+++ src/net/comcraft/src/Mod.java
@@ -1,60 +1,32 @@
 package net.comcraft.src;
+
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
-import javax.microedition.io.Connector;
-import javax.microedition.io.file.FileConnection;
-
-import com.google.minijoe.sys.JsFunction;
 import com.google.minijoe.sys.JsObject;
+import com.google.minijoe.sys.JsFunction;
+
 public class Mod {
 
-    private String info = "Mod Info";
-	private String ModName = "";
-    private String MainClass = "";
-    private String ModDescription = "";
-    private boolean running=false;
+    private String info = "No Mod Info";
+    private String ModName;
+    private String ModDescription;
+    private boolean running = false;
     public boolean enabled = false;
-    public Mod(String path) {
-    	if (true) { // not disabled
-    		enabled=true;
-    	}
-        System.out.println("scanning folder "+path);
-        try {
-            FileConnection fileConnection = (FileConnection) Connector.open(path + "ccmod.info", Connector.READ);
-            if (!fileConnection.exists()) {
-                return;
-            }
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileConnection.openInputStream()));
-            ModName = bufferedReader.readLine().trim();
-            ModDescription = bufferedReader.readLine().trim();
-            MainClass = bufferedReader.readLine().trim().replace('.', '/')+".jclass";
-            bufferedReader.close();
-            fileConnection.close();
-            System.out.println("Found Mod: "+ModName+"("+ModDescription+") @"+MainClass);
-            FileConnection classfcon = (FileConnection) Connector.open(path + MainClass, Connector.READ);
-            if (!classfcon.exists()) {
-                System.out.println("class file "+MainClass+ " does not exist!");
-                return;
-            }
-            System.out.println("Class file: "+path+MainClass);
-            InputStream i=classfcon.openInputStream();
-            try {
-                JsObject global = new BaseMod();
-                JsFunction.exec(new DataInputStream(i), global);
-                running = true;
-            } catch (Exception e) {
-                e.printStackTrace();
-                info=e.getMessage();
-            }
 
-            i.close();
-            classfcon.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new ComcraftException(ex);
+    public Mod(String name, String desc, DataInputStream dis) {
+        ModName = name;
+        ModDescription = desc;
+        if (true) { // not disabled
+            enabled = true;
+        }
+        JsObject global = new BaseMod();
+        try {
+            JsFunction.exec(dis, global);
+            running = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            info = e.getMessage();
         }
     }
 
@@ -65,11 +37,12 @@ public class Mod {
     public String getModDescription() {
         return ModDescription;
     }
+
     public boolean isRunning() {
         return running;
     }
 
-	public String getModInfo() {
-		return info;
-	}
+    public String getModInfo() {
+        return info;
+    }
 }
--- src/net/comcraft/src/ModLoader.java
+++ src/net/comcraft/src/ModLoader.java
@@ -1,12 +1,16 @@
 package net.comcraft.src;
 import java.util.Vector;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 import javax.microedition.io.Connector;
 import javax.microedition.io.file.FileConnection;
+
 import net.comcraft.client.Comcraft;
 
 public class ModLoader {
-	private float version=0.1f;
+    private float version=0.1f;
     private Comcraft cc;
     private Vector Mods;
     private boolean hasInitialized=false;
@@ -22,25 +26,51 @@ public class ModLoader {
                 return;
             }
             System.out.println("scanning mods folder");
-            FileConnection fileConnection = (FileConnection) Connector.open(cc.settings.getComcraftFileSystem().getPathToFolder("mods/"), Connector.READ);
-            if (!fileConnection.exists()){
+            FileConnection fileConnection = (FileConnection) Connector.open(
+                    cc.settings.getComcraftFileSystem()
+                            .getPathToFolder("mods/"), Connector.READ);
+            if (!fileConnection.exists()) {
                 System.out.println("missing folder '/mods'");
                 return;
             }
-            hasInitialized=true;
+            hasInitialized = true;
 
             Vector elements = FileSystemHelper.getElementsList(fileConnection);
+            fileConnection.close();
 
             for (int i = 0; i < elements.size(); ++i) {
                 String elementName = (String) elements.elementAt(i);
-                System.out.println(elementName);
+                String ModName, ModDescription, MainClass;
                 if (elementName.endsWith("/")) {
-                    Mod mod = new Mod(elementName);
-                    Mods.addElement(mod);
+                    FileConnection modInfFile = open(elementName + "ccmod.info");
+                    DataInputStream dis;
+                    if (!modInfFile.exists()) {
+                        continue;
+                        // ModName="Missing Info";
+                        // ModDescription="Folder found, ccmod.info missing";
+                    } else {
+                        BufferedReader ModInfo = new BufferedReader(new InputStreamReader(modInfFile.openInputStream()));
+                        ModName = ModInfo.readLine().trim();
+                        ModDescription = ModInfo.readLine().trim();
+                        MainClass = ModInfo.readLine().trim().replace('.', '/');
+                        ModInfo.close();
+                        FileConnection classfile;
+                        classfile = open(elementName + MainClass + ".ccm");
+                        if (!classfile.exists()) {
+                            classfile.close();
+                            classfile = open(elementName + MainClass + ".jclass");
+                        }
+                        if (!classfile.exists()) {
+                            classfile.close();
+                            continue;
+                        }
+                        dis = new DataInputStream(classfile.openInputStream());
+                    }
+                    modInfFile.close();
+                    Mods.addElement(new Mod(ModName, ModDescription, dis));
                 }
             }
 
-            fileConnection.close();
         } catch (IOException ex) {
             ex.printStackTrace();
             throw new ComcraftException(ex);
@@ -51,5 +81,8 @@ public class ModLoader {
     }
     public boolean isInitialized() {
         return hasInitialized;
+    }
+    private FileConnection open(String filename) throws IOException {
+        return (FileConnection) Connector.open(filename, Connector.READ);
     }
 }
