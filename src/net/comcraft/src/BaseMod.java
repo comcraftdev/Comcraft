c887a1c08112881960549c786ba8b246de155253
simon816 <simonbarnes1@googlemail.com>
2013-10-19 22:56:02 +0100

    Ports AxisAlignedBB to Javascript API

--- src/net/comcraft/src/BaseMod.java
+++ src/net/comcraft/src/BaseMod.java
@@ -1,4 +1,4 @@
-//CCMLGen: 11.1000,16,23
+//CCMLGen: 9.1000,18,26
 package net.comcraft.src;
 
 import com.google.minijoe.sys.JsArray;
@@ -6,6 +6,7 @@ import com.google.minijoe.sys.JsObject;
 
 class BaseMod extends JsObject {
     static final JsObject PROTOTYPE = new JsObject(OBJECT_PROTOTYPE);
+    private static final int ID_AXISALIGNEDBB = 1000;
     JsArray stack = new JsArray();
 
     public BaseMod() {
@@ -14,11 +15,15 @@ class BaseMod extends JsObject {
         addVar("Console", new DebugConsole());
         addVar("EventHandler", ModGlobals.event);
         addVar("Model", new ModelsList());
+        addNative("AxisAlignedBB", ID_AXISALIGNEDBB, 6);
         stack.setObject(0, this);
     }
 
     public void evalNative(int id, JsArray stack, int sp, int parCount) {
         switch (id) {
+        case ID_AXISALIGNEDBB:
+            stack.setObject(sp, new AxisAlignedBB((float) stack.getNumber(sp + 2), (float) stack.getNumber(sp + 3), (float) stack.getNumber(sp + 4), (float) stack.getNumber(sp + 5), (float) stack.getNumber(sp + 6), (float) stack.getNumber(sp + 7)));
+            break;
         default:
             super.evalNative(id, stack, sp, parCount);
         }
